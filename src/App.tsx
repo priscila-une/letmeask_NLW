dd26cad1ae9a48011a8738039858bc8457efb0c8
Priscila Une <prixlin@gmail.com>
2021-06-22 17:24:37 -0300

    day 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import { Button } from "./components/Button";
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import { NewRoom } from "./pages/NewRoom";
+import { Home } from "./pages/Home";
+
+import { AuthContextProvider } from './contexts/AuthContext';
+
+
 
 function App() {
+ 
   return (
-    <div>
-    <h1>Hello World</h1>
-    <Button text="botão 1"/>
-    <Button />
-    <Button />
-    </div>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home}/>
+        <Route path="/rooms/new" component={NewRoom}/>
+        </AuthContextProvider>
+    </BrowserRouter>
+   
   );
 }
 
